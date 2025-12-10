02aba5bbc59a6827a3319d849cf7bec592cd7f3e
sabijoyhacks@gmail.com <sabijoyhacks@gmail.com>
2025-12-10 23:54:40 +0600

    Progress

--- Practice 2.py
+++ Practice 2.py
@@ -1,1 +1,22 @@
 print ("Hello World")
+a = 2
+b = 8
+if a < b:
+    print ("a is less than b")
+else:
+    print ("a is not less than b")
+
+d = int(input("Enter a number: "))
+c = int(input("Enter another number: "))
+e = d + c
+print ("The sum of the two numbers is: ", e)
+
+x, y, z = map(int, input("Enter three numbers: ").split())
+if x < y and x < z:
+    print ("x is the smallest number")
+elif y < x and y < z:
+    print ("y is the smallest number")
+else:
+    print ("z is the smallest number")
+
+
